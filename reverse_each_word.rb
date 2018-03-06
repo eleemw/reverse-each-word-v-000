f42be59bbccb27584831cd1f38d250b9e3158736
Erica Murdock-Waters <ericahpa@gmail.com>
2018-03-06 14:45:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
-#def reverse_each_word(sentence)
-  #reverse_sentence = []
-#  sentence.split.each do |word|
-#    reverse_sentence << word.reverse
-#  end
-#  reverse_sentence.join(" ")
-#end
+def reverse_each_word(sentence)
+  reverse_sentence = []
+  sentence.split.each do |word|
+    reverse_sentence << word.reverse
+  end
+  reverse_sentence.join(" ")
+end
 
 def reverse_each_word(sentence)
   sentence_array = [(sentence.split).collect {|word| word.reverse}]
